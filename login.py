91b3699fac562dcf96fa92d6bdbccaea1743cc83
LuisdelaG21 <luisdxgauss2164@gmail.com>
2026-02-12 19:12:28 -0600

    Inicio de trabajo con GitKraken

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
 print("Login V3!!")
 print("He modificado este archivo para después mezclarlo con la rama feature-menu")
 print("Otra moficiación para usar $git pull origin main desde la rama feature-menu")
+print("Ahora trabajaremos con GitKraken como herramienta visual para Git")
